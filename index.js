70ff19a3c40d094135ceb4130b345fc05d00fce1
Jameel <108789288+Jameel-Algharabat@users.noreply.github.com>
2023-05-19 01:44:22 +0300

    site

--- index.js
+++ index.js
@@ -22,3 +22,90 @@ function sendMail() {
 
 }
 
+
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: new google.maps.LatLng(38.769671, -9.097975),
+        mapTypeId: 'roadmap',
+        styles :
+            [
+                {
+                    "featureType": "all",
+                    "stylers": [
+                        {
+                            "saturation": 0
+                        },
+                        {
+                            "hue": "#e7ecf0"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "saturation": -70
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        },
+                        {
+                            "saturation": -60
+                        }
+                    ]
+                }
+            ]
+
+    });
+
+
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var icons = {
+        info: {
+            icon: 'https://novo2.rhp.pt/map.png'
+        }
+    };
+
+    var features = [
+        {
+            position: new google.maps.LatLng(38.769671, -9.097975),
+            type: 'info'
+        }
+    ];
+
+    // Create markers.
+    features.forEach(function(feature) {
+        var marker = new google.maps.Marker({
+            position: feature.position,
+            icon: icons[feature.type].icon,
+            map: map,
+            content:'<div>Test</div>'
+
+        });
+    });
+}
+
